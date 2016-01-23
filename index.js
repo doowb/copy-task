1081b3d3945630efd26f9a261586441d7f8ec2ee
Brian Woodward <brian.woodward@gmail.com>
2016-01-22 23:47:44 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * copy-task <https://github.com/doowb/copy-task>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
